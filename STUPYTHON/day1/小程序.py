49dbc82c2245ce60d64c9e39b4edef4f83a82d55
yellowhairse <54492161+yellowhairse@users.noreply.github.com>
2019-12-28 15:08:38 +0800

    Study

---
+++ STUPYTHON/day1/小程序.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/12/28 15:01
+# @Author  : wtong
+# @Site    :
+# @File    : 小程序.py
+# @Software: PyCharm
+
+
+
+
+def get_FtoCel(fahrenheit):
+    #F = float(input("华氏温度为："))
+    F = float(fahrenheit)
+    return ("摄氏温度为%.2f" % ((F - 32)/1.8))
+
+
